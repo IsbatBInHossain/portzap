67756791c0e8fc753cf5fa93dedd81de68887658
Isbat-Bin-Hossain <isbat31416@gmail.com>
2025-07-25 09:51:17 +0600

    chore: remove info command in favor of list --port command

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
-import { program } from 'commander'
-import { listCommand } from '../src/commands/listCommand.js'
-import { zapCommand } from '../src/commands/zapCommand.js'
-import { infoCommand } from '../src/commands/infoCommand.js'
+import { program } from 'commander';
+import { listCommand } from '../src/commands/listCommand.js';
+import { zapCommand } from '../src/commands/zapCommand.js';
 
 program
   .name('portzap')
   .description(
     'A simple CLI tool for managing and killing processes on specific ports'
   )
-  .version('0.0.1-alpha')
+  .version('0.0.1-alpha');
 
 // Add zap command
-program.addCommand(listCommand)
-program.addCommand(zapCommand)
-program.addCommand(infoCommand)
+program.addCommand(listCommand);
+program.addCommand(zapCommand);
 
-program.parse()
+program.parse();
